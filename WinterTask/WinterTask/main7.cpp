3daed6d3684ac4248146143f632e220f1b0ca58c
AndouTsudumi <55andouTsudumi25@gmail.com>
2022-11-14 15:49:36 +0900

    問題７の追加
    完了済み

---
+++ WinterTask/WinterTask/main7.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+const int KAMI = 13195;
+const int HUTU = 100;
+const long long KUSO = 600851475143;
+
+const long long DAIHYOU = KUSO;
+
+int main()
+{
+    int ans = 1;
+    double temp = 0.0f;
+    long long after = DAIHYOU;
+
+    for (long long i = 2; i < DAIHYOU; i++)
+    {
+        temp = (double)after / i;
+
+        if (temp < 1.0f)
+        {
+            break;
+        }
+
+        if (after % i != 0)
+        {
+            continue;
+        }
+        else
+        {
+            if (ans < i)
+            {
+                ans = i;
+            }
+        }
+        after = temp;
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
